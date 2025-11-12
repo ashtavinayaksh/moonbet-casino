373af719c193ae6b0995e14b7b8e7ce7ec91b8fd
Ambuj Sharma <ambujsharma90@gmail.com>
2025-11-12 17:26:42 +0530

    changes casino page

--- src/pages/Casino.jsx
+++ src/pages/Casino.jsx
@@ -6,7 +6,12 @@ import LiveCasino from "../components/casino/LiveCasino";
 import TrandingSection from "../components/casino/TrandingSection";
 import HeroSection from "../components/sections/HeroSection";
 import HomeRewardsSection from "../components/sections/HomeRewardsSection";
+import ProvidersSection from "../components/sections/ProvidersSection";
 import CasinoCategoryNav from "../components/casino/Casinocategorynav";
+import TrustBadges from "../components/sections/TrustBadges";
+import GameBetsSection from "../components/sections/GameBetsSection";
+import HomeFAQSection from "../components/sections/HomeFAQSection";
+import CryptoPaymentSection from "../components/sections/CryptoPaymentSection";
 
 const Casino = () => {
   return (
@@ -17,6 +22,11 @@ const Casino = () => {
       <TrandingSection />
       <SlotsSection />
       <LiveCasino />
+      <ProvidersSection />
+      <GameBetsSection />
+      <HomeFAQSection />
+      <CryptoPaymentSection />
+      <TrustBadges />
     </div>
   );
 };
